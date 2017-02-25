7a39edc60d64736b6c141e005ebecf8b21b3df04
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2017-02-25 16:41:21 +0900

    Remove the color depending on the status of git because it is slow.

--- setupPrompt.sh
+++ setupPrompt.sh
@@ -5,23 +5,16 @@ source $(dirname $BASH_SOURCE)/color.sh
 setBashPrompt() {
 
   #Get Git status
-  local branch=$(git branch 2> /dev/null | grep '*' | cut -d' ' -f2)
-
-  local gitColor=$COLOR_GREEN
-  if [ "$(git status --porcelain 2> /dev/null)" != "" ]
+  local gitBranch=$(git branch 2> /dev/null | grep '*' | cut -d' ' -f2)
+  if [ "$gitBranch" != "" ]
   then
-    gitColor=$COLOR_RED
-  fi
-
-  if [ "$branch" != "" ]
-  then
-    local gitStatus="\[${gitColor}\]($branch)\[${COLOR_END}\]"
+    gitBranch="\[${COLOR_GREEN}\]($gitBranch)\[${COLOR_END}\]"
   fi
 
   #\u -> user id
   #\h -> hostname
   #\w -> working directory
-  PS1="\[${COLOR_CYAN}\]\u\[${COLOR_END}\]@\[${COLOR_MAGENTA}\]\h\[${COLOR_END}\]:\[${COLOR_YELLOW}\]\w\[${COLOR_END}\]${gitStatus}\n$ "
+  PS1="\[${COLOR_CYAN}\]\u\[${COLOR_END}\]@\[${COLOR_MAGENTA}\]\h\[${COLOR_END}\]:\[${COLOR_YELLOW}\]\w\[${COLOR_END}\]${gitBranch}\n$ "
 }
 
 PROMPT_COMMAND=setBashPrompt
